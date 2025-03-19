0a3a4a745a18cbb89fa39f4243c6d0d4588acaa9
Tejasv-Singh <tejasvhada1@gmail.com>
2025-03-20 00:19:42 +0530

    Updated trading simulator and data

--- TRADING-SIMULATOR.py
+++ TRADING-SIMULATOR.py
@@ -8,3 +8,17 @@ data.to_csv("AAPL_data.csv")
 
 print(data.head())
 
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(12,6))
+plt.plot(data['Close'], label="Closing Price", color='blue')
+plt.xlabel("Date")
+plt.ylabel("Price (USD)")
+plt.title(f"{stock} Stock Price Over Time")
+plt.legend()
+plt.savefig("stock_plot.png")
+print("Chart saved as stock_plot.png")
+
+
+#Adding moving averages
+
